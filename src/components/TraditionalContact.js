6e322a7fee6e4e41d51566b75490b18d88bc9e46
Jakub Jaskuła <jjcreator8@gmail.com>
2020-05-04 18:13:46 +0200

    minor style changes

--- src/components/TraditionalContact.js
+++ src/components/TraditionalContact.js
@@ -7,17 +7,18 @@ function TraditionalContact() {
             <h2>Call us</h2>
             <div id={styles.twoColumn}>
                 <div className={styles.column}>
+                    <h3>Headquarters</h3>
                     <ul>
-                        <li><span className={styles.bold}>Our office:</span> 123 Fake Street, 77-777 Neverland</li>
-                        <li><span className={styles.bold}>Phone:</span> 0-700-123-Fake</li>
-                        <li><span className={styles.bold}>Fax:</span> 0-700-123-FakeFax</li>
+                        <li><i class="fas fa-map-pin"></i><span className={styles.bold}>Our office:</span> 123 Fake Street, 77-777 Neverland</li>
+                        <li><i class="fas fa-phone"></i><span className={styles.bold}>Phone:</span> 0-700-123-Fake</li>
+                        <li><i class="fas fa-fax"></i><span className={styles.bold}>Fax:</span> 0-700-123-FakeFax</li>
                     </ul>
                 </div>
                 <div className={styles.column}>
                     <h3>Business Hours</h3>
                     <ul>
-                        <li>Monday - Saturday <span className={styles.bold}>9am - 5.30pm</span></li>
-                        <li>Sunday: <span className={styles.bold}>Closed</span></li>
+                        <li><i class="fas fa-clock"></i>Monday - Saturday: <span className={styles.bold}>9am - 5.30pm</span></li>
+                        <li><i class="fas fa-clock"></i>Sunday: <span className={styles.bold}>Closed</span></li>
                     </ul>
                 </div>
             </div>
