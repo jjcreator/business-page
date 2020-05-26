c393cdd52de39bb327654372c8d6dff2ae8f1491
Jakub Jaskuła <jjcreator8@gmail.com>
2020-05-26 10:58:27 +0200

    about page responsive changes

--- src/components/AboutPage/AboutColumns.js
+++ src/components/AboutPage/AboutColumns.js
@@ -4,8 +4,8 @@ import styles from "../../modules/aboutStyle.module.css"
 function AboutColumns() {
     return (
         <div className={`${styles.flex} ${styles.animated}`} id={styles.aboutColumns}>
+            <img src="/team2.jpg" alt="a team of people" id={styles.teamImg}/>
             <div id={styles.aboutText}><span>At vero</span> eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</div>
-            <div id={styles.teamImg}/>
          </div>
     )
 }
