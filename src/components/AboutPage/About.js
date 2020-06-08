0214812c652ffc80826916025be7ddd4545eac29
Jakub Jaskuła <jjcreator8@gmail.com>
2020-06-08 14:13:53 +0200

    changed folder structure, moved imgs to src

--- src/components/AboutPage/About.js
+++ src/components/AboutPage/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Shape from "../Shape"
 import styles from "../../modules/aboutStyle.module.css"
 import AboutColumns from './AboutColumns'
-// import waveImg from "./wave1.png"
+import waveImg from "./images/wave1.png"
 
 function About() {
     
@@ -10,7 +10,7 @@ function About() {
         <div className={`${styles.flex} ${styles.animated}`} id={styles.aboutMainPage}>
             <Shape text="About us"/>
             <AboutColumns/>
-            <img id={styles.waveBottom} src="./wave1.png" alt="wave img"/>
+            <img id={styles.waveBottom} src={waveImg} alt="wave img"/>
         </div>
     )
 }
