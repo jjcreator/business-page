984f25e06da63047943a8c82fb86226dbbd53adb
Jakub Jaskuła <jjcreator8@gmail.com>
2020-06-08 11:00:49 +0200

    Updates

---
+++ precache-manifest.c00025a33be40ac784e56e5381e9bb4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4884b26679cbf17a2690a1aa7536dcb",
+    "url": "/React-Business-Page/index.html"
+  },
+  {
+    "revision": "369104627fef29d4a450",
+    "url": "/React-Business-Page/static/css/main.b68da15b.chunk.css"
+  },
+  {
+    "revision": "aff3465ec126bb97e5c2",
+    "url": "/React-Business-Page/static/js/2.0cd3ec19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Business-Page/static/js/2.0cd3ec19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369104627fef29d4a450",
+    "url": "/React-Business-Page/static/js/main.e03c143b.chunk.js"
+  },
+  {
+    "revision": "5eca488b29b3570e1964",
+    "url": "/React-Business-Page/static/js/runtime-main.5673590c.js"
+  }
+]);
